3e539d0663df75256844040ec2b6dee04e7075cb
TheMostUpset <themostupset@gmail.com>
2024-06-11 13:06:41 +0500

    small improvement

--- gamemodes/serioussam_dm/gamemode/cl_init.lua
+++ gamemodes/serioussam_dm/gamemode/cl_init.lua
@@ -44,8 +44,12 @@ function GM:InitPostEntity()
 end
 
 function GM:PlayerBindPress(ply, bind, pressed)
-	if bind == "+duck" and !ply:IsOnGround() or bind == "+jump" and ply:Crouching() then
-		return true
+    if not pressed then return end
+    bind = bind:lower()
+	if ply:Alive() then
+		if bind == "+duck" and !ply:IsOnGround() or bind == "+jump" and ply:Crouching() then
+			return true
+		end
 	end
 end
 
