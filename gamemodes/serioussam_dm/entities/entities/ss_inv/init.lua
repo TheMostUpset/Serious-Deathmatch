2ae9d22832b297c176b4851b81877bc7ca03925b
wico. <wicodot@yandex.ru>
2024-06-20 18:23:21 +0700

    removed useless init functions

--- gamemodes/serioussam_dm/entities/entities/ss_inv/init.lua
+++ gamemodes/serioussam_dm/entities/entities/ss_inv/init.lua
@@ -3,24 +3,6 @@ include('shared.lua')
 
 ENT.model = "models/serioussam/powerups/invisibility.mdl"
 
-function ENT:Initialize()
-
-	self:SetModel(self.model)
-	self:SetMoveType(MOVETYPE_NONE)
-	self:SetSolid(SOLID_NONE)
-	self:SetAngles(Angle(0,90,0))
-	self:DrawShadow(true)
-	self:SetCollisionGroup(COLLISION_GROUP_DEBRIS)
-	self:SetRenderMode(RENDERMODE_TRANSALPHA)
-	self:SetTrigger(true)
-	self:UseTriggerBounds(true, 24)
-	self.PDuration = 30
-	self:SetModelScale( self:GetModelScale() * 1.25, 1 )
-	
-    self.StartPos = self:GetPos()
-
-end
-
 local function PowerupActive(ply)
 	return ply.SSPowerups and ply.SSPowerups.Invisibility	
 end
@@ -47,18 +29,3 @@ function ENT:Pickup(ent)
 		ent:SetNW2Bool( "HasInvis", false )
 	end)
 end
-
-hook.Add("PlayerPostThink", "SSPowerups_Invisibility", function(ply)
-	if !PowerupActive(ply) then return end
-	
-	-- if ply.SSPowerups.InvisibilityOut and ply.SSPowerups.InvisibilityOut <= CurTime() then
-		-- ply.SSPowerups.InvisibilityOut = nil
-		-- if ply.inv3 then ply.inv3:Stop() end
-	-- end	
-	
-	if ply.SSPowerups.Invisibility <= CurTime() then
-		ply.SSPowerups.Invisibility = nil
-		--ply:SetNoDraw(false)
-
-	end
-end)
--- gamemodes/serioussam_dm/entities/entities/ss_protect/init.lua
+++ gamemodes/serioussam_dm/entities/entities/ss_protect/init.lua
@@ -3,24 +3,6 @@ include('shared.lua')
 
 ENT.model = "models/serioussam/powerups/invulnerability.mdl"
 
-function ENT:Initialize()
-
-	self:SetModel(self.model)
-	self:SetMoveType(MOVETYPE_NONE)
-	self:SetSolid(SOLID_NONE)
-	self:SetAngles(Angle(0,90,0))
-	self:DrawShadow(true)
-	self:SetCollisionGroup(COLLISION_GROUP_DEBRIS)
-	self:SetRenderMode(RENDERMODE_TRANSALPHA)
-	self:SetTrigger(true)
-	self:UseTriggerBounds(true, 24)
-	self.PDuration = 30
-	self:SetModelScale( self:GetModelScale() * 1.25, 1 )
-	
-    self.StartPos = self:GetPos()
-
-end
-
 local function PowerupActive(ply)
 	return ply.SSPowerups and ply.SSPowerups.Protect	
 end
@@ -45,19 +27,6 @@ function ENT:Pickup(ent)
 		ent:SetNW2Bool( "HasProtect", false )
 	end)
 end
-
-hook.Add("PlayerPostThink", "SSPowerups_Protect", function(ply)
-	if !PowerupActive(ply) then return end
-	
-	-- if ply.SSPowerups.ProtectOut and ply.SSPowerups.ProtectOut <= CurTime() then
-		-- ply.SSPowerups.ProtectOut = nil
-
-	-- end	
-	
-	if ply.SSPowerups.Protect <= CurTime() then
-		ply.SSPowerups.Protect = nil
-	end
-end)
 
 hook.Add("PlayerShouldTakeDamage", "SSPowerups_Protect", function(ply)
 	if !PowerupActive(ply) then return end
