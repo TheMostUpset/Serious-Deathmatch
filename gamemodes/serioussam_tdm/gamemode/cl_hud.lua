c4560b546f69c72a27a1af40b961b28ce2413f23
wico. <wicodot@yandex.ru>
2024-07-06 04:11:09 +0700

    player table little tweak

--- gamemodes/serioussam_tdm/gamemode/cl_hud.lua
+++ gamemodes/serioussam_tdm/gamemode/cl_hud.lua
@@ -74,18 +74,19 @@ function playerTable:Paint(w, h)
 
     self.Players = GAMEMODE:GetPlayersSortedByFrags()
 
-	local posX = ScrW() / 2 / 1.215
-    local posY = 10
+
 	local hudr, hudg, hudb = GAMEMODE:GetHUDColorFrame()
-
-
+	local posY = 10
+	local posX = ScrW() / 2 / 1.2
+	
     for _, ply in ipairs(self.Players) do
 		local nick, frags, deaths = ply:Nick(), ply:Frags(), ply:Deaths()
-		
-		draw.SimpleText(nick, "Scoreboard_Font", posX + 1, posY + 1, color_black, TEXT_ALIGN_RIGHT)
+		local frags_text = surface.GetTextSize(frags)
+
+		draw.SimpleText(nick, "Scoreboard_Font", posX + 1 - ply:Frags() - ply:Deaths(), posY + 1, color_black, TEXT_ALIGN_RIGHT)
 		draw.SimpleText(frags .. "  /  " .. deaths, "Scoreboard_Font", ScrW() /2  / 1.04  + 1, posY +1, color_black, TEXT_ALIGN_RIGHT)
 		
-		draw.SimpleText(nick, "Scoreboard_Font", posX, posY, Color(hudr, hudg, hudb), TEXT_ALIGN_RIGHT)
+		draw.SimpleText(nick, "Scoreboard_Font", posX - ply:Frags() - ply:Deaths(), posY, Color(hudr, hudg, hudb), TEXT_ALIGN_RIGHT)
 		if ply == LocalPlayer() then
 		draw.SimpleText(frags .. "  /  " .. deaths, "Scoreboard_Font", ScrW() /2 / 1.04 , posY, Color(hudr, hudg, hudb), TEXT_ALIGN_RIGHT)
 		else
