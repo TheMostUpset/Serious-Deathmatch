a654a524c8da39fa0766f0401cc9d508f4e6ab59
wico. <wicodot@yandex.ru>
2024-08-06 14:51:19 +0700

    blockers

--- gamemodes/serioussam_dm/gamemode/init.lua
+++ gamemodes/serioussam_dm/gamemode/init.lua
@@ -1,3 +1,5 @@
+resource.AddWorkshop("3127352943")
+
 AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("cl_hud.lua")
 AddCSLuaFile("cl_fonts.lua")
@@ -733,3 +735,42 @@ end
 	
 	
 end )]]
+
+local spawnpointmin = Vector( -16, -16, 0 )
+local spawnpointmax = Vector( 16, 16, 64 )
+
+function GM:IsSpawnpointSuitable( pl, spawnpointent, bMakeSuitable )
+
+	local Pos = spawnpointent:GetPos()
+
+	-- Note that we're searching the default hull size here for a player in the way of our spawning.
+	-- This seems pretty rough, seeing as our player's hull could be different.. but it should do the job
+	-- (HL2DM kills everything within a 128 unit radius)
+	if ( pl:Team() == TEAM_SPECTATOR ) then return true end
+
+	local Blockers = 0
+	for k, v in ipairs( ents.FindInBox( Pos + spawnpointmin, Pos + spawnpointmax ) ) do
+		if ( IsValid( v ) && v != pl && v:GetClass() == "player" && v:Alive() ) then
+
+			Blockers = Blockers + 1
+			
+			if ( bMakeSuitable ) then
+				v:SetCollisionGroup( COLLISION_GROUP_PASSABLE_DOOR )
+				v:SetAvoidPlayers(true)
+			end
+			
+			
+			--[[
+			if ( bMakeSuitable ) then
+				v:Kill()
+			end
+			--]]
+			
+		end
+	end
+
+	if ( bMakeSuitable ) then return true end
+	if ( Blockers > 0 ) then return false end
+	return true
+
+end
