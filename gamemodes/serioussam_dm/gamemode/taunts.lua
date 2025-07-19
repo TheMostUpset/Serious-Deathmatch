56b8a41fa9221b1b39f3e430e1adf0892e8e0c14
wico. <wicodot@yandex.ru>
2025-07-20 00:09:48 +0700

    add more taunts

--- gamemodes/serioussam_dm/gamemode/taunts.lua
+++ gamemodes/serioussam_dm/gamemode/taunts.lua
@@ -3,7 +3,11 @@ local neutraltaunts = {
     "quotes/serioussam/taunt02.wav",
     "quotes/serioussam/taunt03.wav",
     "quotes/serioussam/taunt04.wav",
-    "quotes/serioussam/taunt05.wav"
+    "quotes/serioussam/taunt05.wav",
+	"quotes/serioussam/taunt06.wav",
+	"quotes/serioussam/taunt07.wav",
+	"quotes/serioussam/taunt08.wav",
+	"quotes/serioussam/taunt09.wav"
 }
 
 local deathtaunts = {
@@ -11,7 +15,11 @@ local deathtaunts = {
     "quotes/serioussam/deathtaunt02.wav",
     "quotes/serioussam/deathtaunt03.wav",
     "quotes/serioussam/deathtaunt04.wav",
-    "quotes/serioussam/deathtaunt05.wav"
+    "quotes/serioussam/deathtaunt05.wav",
+	"quotes/serioussam/deathtaunt06.wav",
+	"quotes/serioussam/deathtaunt07.wav",
+	"quotes/serioussam/deathtaunt08.wav",
+	"quotes/serioussam/deathtaunt09.wav"
 }
 
 local killtaunts = {
@@ -19,7 +27,11 @@ local killtaunts = {
     "quotes/serioussam/killtaunt02.wav",
     "quotes/serioussam/killtaunt03.wav",
     "quotes/serioussam/killtaunt04.wav",
-    "quotes/serioussam/killtaunt05.wav"
+    "quotes/serioussam/killtaunt05.wav",
+	"quotes/serioussam/killtaunt06.wav",
+	"quotes/serioussam/killtaunt07.wav",
+	"quotes/serioussam/killtaunt08.wav",
+	"quotes/serioussam/killtaunt09.wav",
 }
 
 local lastKillTime = {}
@@ -40,14 +52,14 @@ hook.Add("PlayerButtonDown", "PlayRandomTaunts", function(ply, button)
 		local killTime = lastKillTime[ply:SteamID()]
 		
 		--time check
-		if lastTauntTime[ply:SteamID()] and CurTime() - lastTauntTime[ply:SteamID()] <= 2.5 then
+		if lastTauntTime[ply:SteamID()] and CurTime() - lastTauntTime[ply:SteamID()] <= 3 then
             return
         end
 		
 		lastTauntTime[ply:SteamID()] = CurTime()
 		
 		if ply:Alive() then
-			if killTime and CurTime() - killTime <= 2.5 then
+			if killTime and CurTime() - killTime <= 2 then
 				ply:EmitSound(kill)
 			else
 				ply:EmitSound(neutral)
