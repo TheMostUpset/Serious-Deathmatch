07f9fc829027971f7fe1a89638c8df862b78d3b0
wico. <wicodot@yandex.ru>
2025-07-27 13:30:00 +0700

    adjust the pitch of footsteps

--- gamemodes/serioussam_dm/gamemode/cl_footsteps.lua
+++ gamemodes/serioussam_dm/gamemode/cl_footsteps.lua
@@ -1,5 +1,5 @@
 hook.Add("PlayerFootstep", "CustomFootsteps_SeriousSam", function(ply, pos, foot, soundName, volume, filter)
     local customSoundName = string.gsub(soundName, "player/footsteps", "player/serioussam/footsteps")
-    ply:EmitSound(customSoundName, 87.5, 100, 1, CHAN_AUTO)
+    ply:EmitSound(customSoundName, 87.5, 75, 0.75, CHAN_AUTO)
     return true
 end)
